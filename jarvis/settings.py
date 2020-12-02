1b519fc999711826c074f8b38c7d056a7f437f26
AnubhavMadhav <201851024@iiitvadodara.ac.in>
2020-12-02 13:13:36 +0530

    Procfile Added

--- jarvis/settings.py
+++ jarvis/settings.py
@@ -131,4 +131,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
