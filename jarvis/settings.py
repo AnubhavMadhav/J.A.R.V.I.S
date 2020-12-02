b1d9b11f2ebe37686cc7cade8bd0c6d08d6848bc
AnubhavMadhav <201851024@iiitvadodara.ac.in>
2020-12-02 13:25:10 +0530

    hope

--- jarvis/settings.py
+++ jarvis/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 from . email_info import EMAIL_HOST, EMAIL_HOST_PASSWORD, EMAIL_HOST_USER, EMAIL_PORT, EMAIL_USE_TLS, SEC_KEY
